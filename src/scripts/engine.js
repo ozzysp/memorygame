5be31d80c07984cfe4a1c3356d33da0e891ee6af
Ozzy Scuba <ozzysp@icloud.com>
2023-12-01 16:16:23 -0300

    General updates

---
+++ src/scripts/engine.js
@@ -0,0 +1,30 @@
+const emojis = [
+    "🎧",
+    "🎧",
+    "🎤",
+    "🎤",
+    "🎚️",
+    "🎚️",
+    "🥁",
+    "🥁",
+    "🎹",
+    "🎹",
+    "🎸",
+    "🎸",
+    "🔔",
+    "🔔",
+    "🪈",
+    "🪈",
+];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for(let i=0; i < emojis.length; i++)
+{
+    let box =document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
